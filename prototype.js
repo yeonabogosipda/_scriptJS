426e4a79f9a3ba037fb9fa933bc5d3fc87088859
yeonabogosipda <49853597+yeonabogosipda@users.noreply.github.com>
2021-02-15 20:25:40 +0900

    Create prototype.js

---
+++ prototype.js
@@ -0,0 +1,28 @@
+function Person() {
+  this.eyes = 2;
+  this.nose = 1;
+}
+var kim  = new Person();
+var park = new Person();
+console.log(kim.eyes);  // => 2
+console.log(kim.nose);  // => 1
+console.log(park.eyes); // => 2
+console.log(park.nose); // => 1
+// kim과 park은 eyes와 nose를 공통적으로 가지고 있는데, 메모리에는 eyes와 nose가 두 개씩 총 4개 할당됩니다. 객체를100개 만들면 200개의 변수가 메모리에 할당되겠죠?
+// 바로 이런 문제를 프로토타입으로 해결할 수 있습니다.
+
+
+function Person() {}
+Person.prototype.eyes = 2;
+Person.prototype.nose = 1;
+var kim  = new Person();
+var park = new Person():
+console.log(kim.eyes); // => 2
+...
+자바스크립트 개발을 하시는 분이라면 아마 써보진 않았어도 최소한 본 적은 있을겁니다. 
+간단히 설명하자면 Person.prototype이라는 빈 Object가 어딘가에 존재하고, Person 함수로부터 생성된 객체(kim, park)들은 어딘가에 존재하는 Object에 들어있는 값을 모두 갖다쓸 수 있습니다.
+즉, eyes와 nose를 어딘가에 있는 빈 공간에 넣어놓고 kim과 park이 공유해서 사용하는 것이죠. 
+이해되셨나요?
+프로토타입을 깊게 파보면 엄청나게 복잡하지만 개발자가 사용하는 부분만 본다면 이게 거의 전부입니다. 
+하지만 개발자는 사용법만 알고있는게 아니라 언제나 왜? 를 생각해야합니다.
+
