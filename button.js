a1c0b868cec44e546be2b4901f0feeec1cb79ca0
yeonabogosipda <justlikethat2@outlook.kr>
2021-03-02 04:52:28 +0000

    .gitignore

--- button.js
+++
@@ -1,8 +0,0 @@
-<script>
-var buttons = document.querySelectorAll(".check");
-		for(var i=0; i<buttons.length; i++) {
-			buttons[i].addEventListener("click", function() {
-				this.parentNode.style.color = "#ccc";
-			});
-		}
-</script>
